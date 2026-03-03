fc5625bab32baacc136c5b623a54a37534ab1aad
Jesus Oswaldo Muñiz Martinez <22030685@itcelaya.edu.mx>
2026-03-03 11:47:16 -0600

    feat: integración de UI y lógica de perfiles dinámica

---
+++ script.js
@@ -0,0 +1,35 @@
+const colors = ["#0a0a0a", "#1a0000", "#001a00", "#00001a", "#1a1a00"];
+let colorIndex = 0;
+
+const members = [
+    { photo: "yo.jpg", desc: "Oswaldo Muñiz" },
+    { photo: "compa.jpg", desc: "Compañero - Descripción de su rol en el equipo." }
+];
+let memberIndex = 0;
+
+function updateTime() {
+    const timeDisplay = document.getElementById("time-display");
+    const now = new Date();
+    timeDisplay.textContent = "Hora local: " + now.toLocaleTimeString();
+}
+setInterval(updateTime, 1000); // Se actualiza cada segundo
+updateTime(); // Llamada inicial
+
+document.getElementById("colorButton").addEventListener("click", () => {
+    colorIndex = (colorIndex + 1) % colors.length;
+    document.body.style.backgroundColor = colors[colorIndex];
+});
+
+document.getElementById("switchButton").addEventListener("click", (e) => {
+
+    memberIndex = (memberIndex + 1) % members.length;
+
+    document.getElementById("member-photo").src = members[memberIndex].photo;
+    document.getElementById("member-desc").textContent = members[memberIndex].desc;
+
+    if (memberIndex === 0) {
+        e.target.textContent = "Mostrar al compañero";
+    } else {
+        e.target.textContent = "Mostrar a Jesus";
+    }
+});
